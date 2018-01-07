b05c056fec2ab4146c53cadba7447350bca8f629
denissys <denissys@gmail.com>
2018-01-07 16:30:55 -0200

    Creating worker to scan redis keys and transform metrics about client state

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 'use strict'
 
+const clientStateMetricsWorker = require('./lib/workers/client-state-metrics-worker.js');
+
+clientStateMetricsWorker.WorkerBuilder.health(30000).scan('*-20180107-14', function(){
+    console.log('StateMetricWorker aborted, restart your process');
+});
