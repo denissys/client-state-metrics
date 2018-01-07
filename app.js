0e7666ee5260c142c0c8affb3a079152eeb7ac66
denissys <denissys@gmail.com>
2018-01-07 18:52:14 -0200

    Creating components to consolidate metrics

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const clientStateMetricsWorker = require('./lib/workers/client-state-metrics-worker.js');
+const keepAliveMetricsWorker = require('./lib/workers/keep-alive-metrics-worker.js').WorkerBuilder;
 
-clientStateMetricsWorker.WorkerBuilder.health(30000).scan('*-20180107-14', function(){
+keepAliveMetricsWorker.healthOn().scan(function() {
     console.log('StateMetricWorker aborted, restart your process');
 });
