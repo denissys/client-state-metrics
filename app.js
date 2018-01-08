2bc309444a885ab93beb997b399d9d4e664d0874
denissys <denissys@gmail.com>
2018-01-08 02:12:57 -0200

    Improvements - add async functions

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 
 const KeepAliveMetricsWorker = require('./lib/workers/keep-alive-metrics-worker.js').WorkerBuilder;
 
-new KeepAliveMetricsWorker.scan(function() {
+KeepAliveMetricsWorker.scan(function() {
     console.log('StateMetricWorker aborted, restart your process');
 });
